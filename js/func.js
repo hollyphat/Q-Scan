166222b568c75e8109e17cb09d516118a76d55b9
Olatunji <sirhollyfat@gmail.com>
2019-11-02 11:21:04 +0100

    Scanner function added

--- js/func.js
+++ js/func.js
@@ -15,6 +15,31 @@ if (local == "online") {
 	url = "http://projects.apps/app-project-2019/evoting/Fpe_evoting.php";
 }
 
+
+
+var scanApp = {   
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady);
+    },    onDeviceReady: function () {
+        console.log('Received Device Ready Event');
+        Log.initialize(app.displayLogLine);
+    },
+    scan: function () {
+        cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                    alert("Barcode/QR code data\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+        );
+    },
+};
+
+
 function toast_alert(msg){
 	return app.dialog.alert(msg);
 }
